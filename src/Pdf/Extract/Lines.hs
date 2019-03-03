beb76bc71b61db8be554282e257a1e7b1f32acda
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2019-03-03 04:33:17 +0100

    findLinesWindow: Added threshold and drop arguments

--- src/Pdf/Extract/Lines.hs
+++ src/Pdf/Extract/Lines.hs
@@ -9,17 +9,22 @@ import Data.List
 import Pdf.Extract.Glyph
 
 
--- | Collect the glyphs of a page into lines.
+-- | Collect the glyphs of a page into lines. This simple algorithm
+-- works if the glyphs of a line are horizontally oriented. They
+-- needed be lined up exactly, but the line must not be skew or bend
+-- up or down.
 --
--- 1) We construct a window, that spans the pagewidth but is not heigh
+-- 1) Construct a window, that spans the pagewidth but is not heigh
 -- and that steps the height of the page from the bottom to the
--- top. It thereby counts the glyphs at each height. The windows
+-- top. It thereby counts the glyphs at each height. The steps
 -- overlap.
 --
 -- 2) The resulting list of glyphs in each height is then split where
--- the count of glyphs is zero. The result is a list of lists, where
--- each inner list is a sequence of glyphs in a window. Since the
--- windows overlap, each glyph will show up in more than one window.
+-- the count of glyphs falls below a certain threshold. The glyphs in
+-- windows with count lower than the treshold are lost--this may be
+-- wanted in case of dirt. The result is a list of lists, where each
+-- inner list is a sequence of glyphs in a window. Since the windows
+-- overlap, each glyph will show up in more than one window.
 --
 -- 3) We concat these inner lists and then remove the duplicate
 -- glyphs. The result is a list of list of glyphs at similar heights,
@@ -27,12 +32,14 @@ import Pdf.Extract.Glyph
 --
 -- The problem maybe 2, the splitting by zero glyphs at a window
 -- height. The page may be dirty or the lines may be skew.
-findLinesWindow :: (Eq g, Glyph g) => Int -> Int -> [g] -> [[g]]
-findLinesWindow linesCount stepsPerLine glyphs =
+findLinesWindow :: (Eq g, Glyph g) => Int -> Int -> Int -> Bool -> [g] -> [[g]]
+findLinesWindow linesCount stepsPerLine threshold drop glyphs =
   reverse $ -- we started at the bottom, so need to reverse
-  filter ((/=0) . length) $ -- remove empty windows
+  (if drop
+    then filter ((>=threshold) . length)    -- drop windows under threshold
+    else filter ((/=threshold) . length)) $ -- remove empty windows
   map (nub . concat) $ -- concat windows and remove duplicates again
-  splitWhen ((==0) . length) glyphsInWindow -- split by empty windows
+  splitWhen ((<threshold) . length) glyphsInWindow -- split by empty windows
   
   -- filter ((/=0) . length) $ -- remove empty windows
   -- map (nubBy glyphEq . concatMap snd) $ -- concat windows and remove duplicates again
