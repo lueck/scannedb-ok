85c535de9981ca618c2d30ef4285c2f911eab973
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2019-03-09 15:17:48 +0100

    page bottom constantly at 0

--- src/Pdf/Extract/Lines.hs
+++ src/Pdf/Extract/Lines.hs
@@ -57,8 +57,13 @@ findLinesWindow linesCount stepsPerLine threshold drop glyphs =
     step = abs((top - bottom) / fromIntegral(stepsCount - 1))
     window = abs((bottom_ - top_) / fromIntegral(linesCount * linesFactor))
     top = top_ + 1.0
-    bottom = bottom_ - window - 1.0 -- bottom - window - 1
-    bottom_ = glyphsBottom glyphs
+    -- As long as we don't have bbox data from the page, we can asume
+    -- bottom at 0. So when the page isn't filled vertically, the
+    -- window size is still good, provided that the text still starts
+    -- on the head or top of the page. Are there any systems of
+    -- script, where the text starts at the bottom?
+    bottom = 0 -- bottom_ - window - 1.0 -- bottom - window - 1
+    bottom_ = 0 -- glyphsBottom glyphs
     top_ = glyphsTop glyphs
     linesFactor = 3
 
